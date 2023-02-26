7559b318443b5835cbf37b654e0b73673251dd6a
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-02-26 13:24:02 +0530

    project setup

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Frontend Mentor - Accordion Component
+# Online Library
 
 ### Screenshot
 
--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -5,8 +5,8 @@ import './Book.css'
 
 const DUMMY_BOOKS = [
     { id: 'book' + Math.random(), title: 'Game of Thrones', author: 'George .R. Martin', pages: 780, isRead: false},
-    { id: 'book' + Math.random(), title: 'Alice in Wonderland', author: 'Lewis Caroll', pages: 300, isRead: true},
-    { id: 'book' + Math.random(), title: 'Gulliver Travels', author: 'Jonathan Swift', pages: 450, isRead: false },
+    { id: 'book' + Math.random(), title: 'Lord of the Rings', author: 'J. R. R. Tolkien', pages: 300, isRead: true},
+    { id: 'book' + Math.random(), title: 'Harry Potter', author: 'JK Rowling', pages: 450, isRead: false },
 ]
 
 const Book = (props) => {
